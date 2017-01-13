f2e1be1f7e483157e20fab5b3f6ec790015a407d
Dominic Muttel <dominic.muttel@gmail.com>
2017-01-12 18:01:01 -0700

    added multiple clock functionality

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
 function onReady(){
     console.log("I work!");
-    setInterval(updateClock, 1000);
+    var clock = createClock('clock');
+    var clock2 = createClock('clock2');
 
 }
-function updateClock(){
-    var date = new Date();
 
+function createClock(id){
+    var c = new Object();
+        c.updateClock = function(){
+            var date = new Date();
 
-    console.log(date.getHours());
-    console.log(date.getMinutes());
-    console.log(date.getSeconds());
+            // console.log(date.getHours());
+            // console.log(date.getMinutes());
+            // console.log(date.getSeconds());
 
-    var clock = document.getElementById('clock');
-    clock.innerHTML = formatDigits(date.getHours()) + ":" + formatDigits(date.getMinutes()) + ":" + formatDigits(date.getSeconds());
+            var clock = document.getElementById(id);
+            clock.innerHTML = formatDigits(date.getHours()) + ":" + formatDigits(date.getMinutes()) + ":" + formatDigits(date.getSeconds());
+        };
+    setInterval(c.updateClock, 1000);
+    c.updateClock();
+    return c;
 }
+
+// function updateClock(){
+//     var date = new Date();
+//
+//     // console.log(date.getHours());
+//     // console.log(date.getMinutes());
+//     // console.log(date.getSeconds());
+//
+//     var clock = document.getElementById('clock');
+//     clock.innerHTML = formatDigits(date.getHours()) + ":" + formatDigits(date.getMinutes()) + ":" + formatDigits(date.getSeconds());
+// }
+
 function formatDigits(val) {
     if(val<10) {
         val = "0" + val;
     }
     return val;
 }
+
 window.onLoad = onReady();
